fa0b538c8f7fee93162117e7f09eed0b7409836f
Lucas Leandro <lukkassoliveira2215@gmail.com>
2025-09-18 20:49:25 -0300

    removed cpf and birth_date

--- app/controllers/direction/teachers_controller.rb
+++ app/controllers/direction/teachers_controller.rb
@@ -84,7 +84,7 @@ class Direction::TeachersController < ApplicationController
   def user_params
     params.require(:user).permit(
       :email, :password, :password_confirmation, :first_name, :last_name,
-      :phone, :birth_date, :cpf, :registration_number, :position,
+      :phone, :registration_number, :position,
       :specialization, :active
     )
   end
